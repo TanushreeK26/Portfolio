a145e00796a6253658c91d6125e62a0eba577108
Tanushree K <tanushreekrishnaraj@gmail.com>
2025-11-25 17:15:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Smooth scroll glow effect on active section
+const links = document.querySelectorAll('nav a');
+
+
+links.forEach(link => {
+link.addEventListener('click', () => {
+links.forEach(l => l.classList.remove('active'));
+link.classList.add('active');
+});
+});
